d30ac54e84b677eefe23970dc857be20c2404160
Martin Kirchgessner <martin.kirch@gmail.com>
2018-12-24 17:46:42 +0100

    listener script now listens on a UDP port, first attempt

--- listen_now_and_next_and_post_to_wordpress.sh
+++ listen_now_and_next_and_post_to_wordpress.sh
@@ -5,7 +5,7 @@
 # "UDP port" where Rivendell sends its now & next messages
 PORT=2345
 
-# public URL of your wordpress install
+# public URL of your wordpress install - don't forget the / at the end
 WORDPRESS_BASE_URL="http://localhost/"
 
 # secret key, as set in the plugin's parameters
@@ -16,13 +16,29 @@ KEY="sôm&ethi'ng w\\\"eird((--\$))"
 
 ####### DO NOT EDIT BELOW ####################
 
-# TODO grab this with netcat listening on UDP
-ARTISTTITLE="Hahaha test with ' & âccents___Tït\\\"le"
+if ! which ncat &> /dev/null
+then
+    echo "Exiting: this script requires ncat (on Centos, install with sudo yum install nmap-ncat)";
+    exit 1;
+fi
 
-# protect & from URL-encoding
-ARTISTTITLE=${ARTISTTITLE/\&/\%26}
-KEY=${KEY/\&/\%26}
+function post_to_wordpress() {
+    # protect & from URL-encoding
+    ARTISTTITLE=${1/\&/\%26}
+    KEY=${KEY/\&/\%26}
 
-URL="${WORDPRESS_BASE_URL}wp/wp-admin/admin-post.php?action=rivendell_now_and_next_store"
+    URL="${WORDPRESS_BASE_URL}wp/wp-admin/admin-post.php?action=rivendell_now_and_next_store"
 
-curl -X POST -d "key=$KEY" -d "artisttitle=$ARTISTTITLE" $URL
+    curl -X POST -d "key=$KEY" -d "artisttitle=$ARTISTTITLE" $URL
+}
+
+while true
+do
+	line=`ncat -u -l $PORT -i 1s 2> /dev/null`
+	if [ ! -z "$line" ];
+	then
+		post_to_wordpress "$line";
+	fi
+done;
+
+
