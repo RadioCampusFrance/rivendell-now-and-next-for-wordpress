679a6202a14c6c1e900a0a919dd80abb230cdb33
Martin Kirchgessner <martin.kirch@gmail.com>
2018-12-25 17:09:41 +0100

    disable curl logging (except on error), and script clarity

--- listen_now_and_next_and_post_to_wordpress.sh
+++ listen_now_and_next_and_post_to_wordpress.sh
@@ -2,14 +2,14 @@
 
 ##### CHANGE VARIABLES BELOW
 
-# "UDP port" where Rivendell sends its now & next messages
-PORT=2345
-
 # public URL of your wordpress install - don't forget the / at the end
 WORDPRESS_BASE_URL="http://localhost/"
 
 # secret key, as set in the plugin's parameters
 KEY="sôm&ethi'ng w\\\"eird((--\$))"
+
+# "UDP port" where Rivendell sends its now & next messages
+PORT=2345
 
 
 
@@ -29,7 +29,7 @@ function post_to_wordpress() {
 
     URL="${WORDPRESS_BASE_URL}wp/wp-admin/admin-post.php?action=rivendell_now_and_next_store"
 
-    curl -X POST -d "key=$KEY" -d "artisttitle=$ARTISTTITLE" $URL
+    curl -s -S -X POST -d "key=$KEY" -d "artisttitle=$ARTISTTITLE" $URL > /dev/null
 }
 
 while true
